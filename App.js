674d56b3cfa5c6ec3385658b53e3169032b17ea7
Milana46 <139504932+Milana46@users.noreply.github.com>
2024-01-31 00:00:46 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+     
+    </div>
+  );
+}
+
+export default App;

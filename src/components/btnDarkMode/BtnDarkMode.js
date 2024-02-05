1945753a576b2e23e3cd117e59a79aae7b7f884c
Milana46 <139504932+Milana46@users.noreply.github.com>
2024-02-05 03:51:09 +0300

    portSite

---
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import "./style.css";
+import sun from "./../../img/icons/sun.svg";
+import moon from "./../../img/icons/moon.svg";
+import {  useLocalStorage } from "./../utils/useLocalStorage";
+
+const BtnDarkMode = () => {
+  
+  const [darkMode, setDarkMode]=useLocalStorage("darkMode", "Light");
+
+  useEffect(() => {
+    if (darkMode === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((currentValue) => {
+      return currentValue === "light" ? "dark" : "light";
+    });
+  };
+
+  return (
+    <button className="dark-mode-btn" onClick={toggleDarkMode}>
+      <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+      <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+    </button>
+  );
+};
+
+export default BtnDarkMode;
---
+++ src/helpers/projectList.js
@@ -0,0 +1,67 @@
+
+import pic1 from "./../img/projects/01.jpg";
+import pic1Big from "./../img/projects/01-big.jpg";
+
+import pic2 from "./../img/projects/02.jpg";
+import pic2Big from "./../img/projects/02-big.jpg";
+
+import pic3 from "./../img/projects/03.jpg";
+import pic3Big from "./../img/projects/03-big.jpg";
+
+import pic4 from "./../img/projects/04.jpg";
+import pic4Big from "./../img/projects/04-big.jpg";
+
+const projects=[
+
+    {
+        title: "Gaming streaming portal",
+        skills: "React, Node.js, MongoDB",
+        img: pic1,
+        imgBig: pic1Big,
+        gitHubLink: "https://gitHub.com",
+
+    },
+
+    {
+        title: "Video Service",
+        skills: "React, Node.js, PHP",
+        img: pic2,
+        imgBig: pic2Big,
+        gitHubLink: "https://gitHub.com",
+    },
+
+    {
+        title: "Video portal",
+        skills: "React, Node.js, PHP",
+        img: pic3,
+        imgBig: pic3Big,
+        gitHubLink: "https://gitHub.com",
+    },
+
+    {
+        title: "Dating app",
+        skills: "React Native",
+        img: pic4,
+        imgBig: pic4Big,
+        
+    },
+
+    {
+        title: "Dating app",
+        skills: "React Native",
+        img: pic4,
+        imgBig: pic4Big,
+        
+    },
+
+    {
+        title: "Dating app",
+        skills: "React Native",
+        img: pic4,
+        imgBig: pic4Big,
+        
+    },
+
+]
+
+export {projects}
